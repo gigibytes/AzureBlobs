7f82a92fa03d5bc686516b9b31c2e13b20e20451
GiGi <gigi@hurtault.dev>
2024-03-21 19:13:42 -0400

    initialize project and add sample code

---
+++ AzureBlobs/Program.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+
+public BlobServiceClient GetBlobServiceClient(string accountName)
+{
+    BlobServiceClient client = new(
+        new Uri($"https://{accountName}.blob.core.windows.net"),
+        new DefaultAzureCredential());
+    return client;
+}
